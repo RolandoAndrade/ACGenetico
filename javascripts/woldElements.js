272b271eac00b8d8c25d3affebed5b81eaf83935
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-02-10 19:39:34 -0400

    Etiquetas de tierra y agua

--- javascripts/woldElements.js
+++ javascripts/woldElements.js
@@ -1,3 +1,5 @@
+const ELEMENTS_FONT_SIZE=15;
+
 class Ground
 {
     constructor(x,y,w,h)
@@ -11,6 +13,7 @@ class Ground
     draw(ctx)
     {
         new Rectangle(this.x,this.y,this.w, this.h, "#49dc60").draw(ctx);
+        new CanvasText("TIERRA", this.x + this.w/2, this.y + this.h / 2 + ELEMENTS_FONT_SIZE, ELEMENTS_FONT_SIZE,'white').draw(ctx)
     }
 }
 
@@ -27,5 +30,6 @@ class Water
     draw(ctx)
     {
         new Rectangle(this.x,this.y,this.w, this.h, "#23a6dc").draw(ctx);
+        new CanvasText("AGUA", this.x + this.w/2,this.y + this.h / 2 + ELEMENTS_FONT_SIZE, ELEMENTS_FONT_SIZE,'white').draw(ctx);
     }
 }
--- javascripts/world.js
+++ javascripts/world.js
@@ -4,7 +4,7 @@ class World
     {
         this.canvas = document.getElementById('canvas');
         this.ctx = this.canvas.getContext('2d');
-        this.water = new Water(0,0,this.canvas.width, this.canvas.height);
+        this.water = new Water(0,this.canvas.height/2,this.canvas.width, this.canvas.height/2);
         this.ground = new Ground(0,0,this.canvas.width, this.canvas.height/2);
     }
 
