c6df8ccf6f383d03c14f8bc0d8291fec09d64027
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-02-10 19:14:10 -0400

    Añadidos elementos útiles

---
+++ javascripts/world.js
@@ -0,0 +1,13 @@
+class World
+{
+    constructor()
+    {
+        this.canvas = document.getElementById('canvas');
+        this.ctx = this.canvas.getContext('2d');
+    }
+
+    draw()
+    {
+
+    }
+}
